7471284dd9967efbf96594cd31dbc402518015e1
unknown <panterajuliapapina@yandex.ru>
2023-08-15 16:23:00 +0300

    адаптивность стилей promo

---
+++ src/components/AboutProject/AboutProject.js
@@ -0,0 +1,11 @@
+import './AboutProject.css';
+
+function AboutProject() {
+    return (
+        <section className="about-project" id="about">
+            Проект
+        </section>
+    )
+}
+
+export default AboutProject;

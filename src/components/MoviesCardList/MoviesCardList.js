6b1ff07bc0c7d093ec53e0ce3622061939d71096
unknown <panterajuliapapina@yandex.ru>
2023-08-19 16:59:19 +0300

    реализовала разный список карточек на страницах movies и saved-movies

--- src/components/MoviesCardList/MoviesCardList.js
+++ src/components/MoviesCardList/MoviesCardList.js
@@ -1,26 +1,42 @@
-import './MoviesCardList.css';
-import { moviesArray } from '../../utils/moviesArray';
-import MoviesCard from '../MoviesCard/MoviesCard';
-import Preloader from '../Preloader/Preloader';
-import LoadingButton from '../LoadingButton/LoadingButton';
-
+import "./MoviesCardList.css";
+import { moviesArray } from "../../utils/moviesArray";
+import MoviesCard from "../MoviesCard/MoviesCard";
+import LoadingButton from "../LoadingButton/LoadingButton";
+import { useLocation } from "react-router-dom";
+import { moviesSave } from "../../utils/moviesSave";
 
 function MoviesCardList() {
-    return (
-        <section className="movies-card-list">
-             <div className="movies-card-list__container">
-          <div className="movies-card-list__elements">
-            {moviesArray.map((item) => {
-              return <MoviesCard movie={item} key={item.movieId} />;
-            })}
-          </div>
-          <div className="movies-card-list__load">
-          {false ? <Preloader /> : <LoadingButton />}
-        </div>
-          </div>
-        </section>
-      );
+  const { pathname } = useLocation();
 
+  return (
+    <section className="movies-card-list">
+      <div className="movies-card-list__container">
+        <div className="movies-card-list__elements">
+          {pathname === "/movies"
+            ? moviesArray.map((item) => {
+                return (
+                  <MoviesCard movie={item} key={item.movieId} path={pathname} />
+                );
+              })
+            : null}
+        </div>
+
+        <div className="movies-card-list__elements">
+          {pathname === "/saved-movies"
+            ? moviesSave.map((item) => {
+                return (
+                  <MoviesCard movie={item} key={item.movieId} path={pathname} />
+                );
+              })
+            : null}
+        </div>
+
+        <div className="movies-card-list__load">
+          {pathname === "/movies" ? <LoadingButton /> : null}
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default MoviesCardList;
